901987d73fa4c0131f6956bda717f335a5381332
Sharan Khan <sharan@mobinspire.com>
2017-06-15 15:31:48 +0500

    changes made in schema

---
+++ bin/model/agents.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AgentSchema = new Schema({
+	name: String,
+	currentLocation: {
+		type: [Number],
+		index: '2dsphere',
+		default: [0, 0]
+	}
+});
+
+module.exports = mongoose.model('Agent', AgentSchema);
